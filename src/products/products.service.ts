4a32264095cea1040e0617adad90cc88fe94a0b8
Craig Miller <craigmiller160@gmail.com>
2021-04-03 16:42:27 -0400

    Committing setup of module

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './product.model';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class ProductsService {
 	private products: Product[] = [];
 
 	addProduct(product: Product): Product {
+		product.id = nanoid();
 		this.products.push(product);
 		return product;
 	}
