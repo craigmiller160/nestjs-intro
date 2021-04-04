f069d108490e4c5467f4b7b5359174615c01cd18
Craig Miller <craigmiller160@gmail.com>
2021-04-04 16:21:13 -0400

    Committing progress

--- src/interceptors/NoContentInterceptor.ts
+++ src/interceptors/NoContentInterceptor.ts
@@ -1,9 +1,19 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+	CallHandler,
+	ExecutionContext,
+	Injectable,
+	NestInterceptor
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class NoContentInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        return next.handle()
-    }
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const res = context.switchToHttp().getResponse();
+		console.log('Res', res.body); // TODO delete this
+		if (!res.body) {
+			res.status(204);
+		}
+		return next.handle();
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NoContentInterceptor } from './interceptors/NoContentInterceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalInterceptors(new NoContentInterceptor());
 	await app.listen(3000);
 }
 bootstrap();
